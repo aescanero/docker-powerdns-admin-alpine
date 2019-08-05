e9bc2a4e6c1553bd34616b040edfda1b0a2ccfb2
Alejandro Escanero Blanco <aescanero@gmail.com>
2019-08-05 15:36:52 +0200

    Triing to reduce size version

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,26 @@
-FROM alpine:edge
+FROM alpine:3.10
 MAINTAINER "Alejandro Escanero Blanco <aescanero@disasterproject.com>"
 
 RUN apk add --no-cache python3 curl py3-mysqlclient py3-yaml py3-pytest py3-tz py3-openssl py3-gunicorn py3-dnspython py3-pyldap \
   py3-sqlalchemy py3-flask py3-flask-login py3-bcrypt py3-configobj py3-requests py3-virtualenv py3-cryptography \
   py3-mako py3-lxml py3-defusedxml py3-dateutil py3-simplejson py3-webcolors py3-jsonschema \
-  xmlsec mariadb-client \
+  xmlsec mariadb-client yarn\
   python3-dev xmlsec-dev musl-dev libxslt-dev gcc\
-  && adduser -S pdnsadmin && cd /home/pdnsadmin/ \
-  && curl -sSL https://github.com/ngoduykhanh/PowerDNS-Admin/archive/master.tar.gz | tar -xzC /home/pdnsadmin --strip 1 && virtualenv flask \
+  && mkdir -p /opt/pdnsadmin/ && cd /opt/pdnsadmin \
+  && curl -sSL https://github.com/ngoduykhanh/PowerDNS-Admin/archive/master.tar.gz | tar -xzC /opt/pdnsadmin --strip 1 \
   && sed -i -e '/mysqlclient/d' -e '/YAML/d' -e '/pytest/d' -e '/pytz/d' -e '/pyOpenSSL/d' -e '/gunicorn/d' -e '/dnspython/d' \
   -e '/python-ldap/d' -e '/SQLAlchemy/d' -e '/Flask=/d' -e '/Flask-Login=/d' -e '/bcrypt/d' -e '/configobj/d' \
   -e '/requests/d' requirements.txt
 
-RUN cd /home/pdnsadmin && pip3 install --no-cache-dir -r requirements.txt && apk del python3-dev xmlsec-dev musl-dev libxslt-dev gcc
+RUN cd /opt/pdnsadmin && pip3 install --no-cache-dir -r requirements.txt \
+  && cp config_template.py config.py \
+  && mkdir -p app/static/node_modules \
+  && yarn install --pure-lockfile \
+  && virtualenv flask && source ./flask/bin/activate && flask assets build \
+  && apk del python3-dev xmlsec-dev musl-dev libxslt-dev gcc yarn
 
 ADD entrypoint.sh /entrypoint.sh
 RUN chmod u+x /entrypoint.sh
 
+EXPOSE 9191
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,34 @@
 #!/bin/sh
 
-cat >/home/pdnsadmin/config.py <<EOF
+#Based in work from Khanh Ngo "k@ndk.name" (https://github.com/ngoduykhanh/PowerDNS-Admin/blob/master/docker/PowerDNS-Admin/Dockerfile)
+
+DB_MIGRATION_DIR='/opt/pdnsadmin/migrations'
+
+if [ -z ${PDNS_PROTO} ];
+ then PDNS_PROTO="http"
+fi
+
+if [ -z ${PDNS_PORT} ];
+ then PDNS_PORT=8081
+fi
+
+if [ -z ${PDNS_HOST} ];
+ then PDNS_HOST="127.0.0.1"
+fi
+
+if [ -z ${$PDNSADMIN_SQLA_DB_PORT} ];
+ then $PDNSADMIN_SQLA_DB_PORT=3306
+fi
+
+# Wait for us to be able to connect to MySQL before proceeding
+echo "===> Waiting for $PDNSADMIN_SQLA_DB_HOST MySQL service"
+until nc -zv $PDNSADMIN_SQLA_DB_HOST $PDNSADMIN_SQLA_DB_PORT;
+do
+  echo "MySQL ($PDNSADMIN_SQLA_DB_HOST) is unavailable - sleeping"
+  sleep 5
+done
+
+cat >/opt/pdnsadmin/config.py <<EOF
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 BIND_ADDRESS = '0.0.0.0'
@@ -22,28 +50,40 @@ SAML_SIGN_REQUEST = False
 SAML_LOGOUT = False
 EOF
 
-if [ -z $PDNSADMIN_SECRET_KEY ];then echo "SECRET_KEY = 'We are the world'";else echo "SECRET_KEY = '$PDNSADMIN_SECRET_KEY'";fi >>/home/pdnsadmin/config.py
-if [ -z $PDNSADMIN_PORT ];then echo "PORT = 9191";else echo "PORT = $PDNSADMIN_PORT";fi >>/home/pdnsadmin/config.py
-if [ -z $PDNSADMIN_SQLA_DB_USER ];then echo "SQLA_DB_USER = 'pda'";else echo "SQLA_DB_USER = '$PDNSADMIN_SQLA_DB_USER'";fi >>/home/pdnsadmin/config.py
-if [ -z $PDNSADMIN_SQLA_DB_PASSWORD ];then echo "SQLA_DB_PASSWORD = 'changeme'";else echo "SQLA_DB_PASSWORD = '$PDNSADMIN_SQLA_DB_PASSWORD'";fi >>/home/pdnsadmin/config.py
-if [ -z $PDNSADMIN_SQLA_DB_HOST ];then echo "SQLA_DB_HOST = '127.0.0.1'";else echo "SQLA_DB_HOST = '$PDNSADMIN_SQLA_DB_HOST'";fi >>/home/pdnsadmin/config.py
-if [ -z $PDNSADMIN_SQLA_DB_PORT ];then echo "SQLA_DB_PORT = 3306";else echo "SQLA_DB_PORT = $PDNSADMIN_SQLA_DB_PORT";fi >>/home/pdnsadmin/config.py
-if [ -z $PDNSADMIN_SQLA_DB_NAME ];then echo "SQLA_DB_NAME = 'pda'";else echo "SQLA_DB_NAME = '$PDNSADMIN_SQLA_DB_NAME'";fi >>/home/pdnsadmin/config.py
+if [ -z $PDNSADMIN_SECRET_KEY ];then echo "SECRET_KEY = 'We are the world'";else echo "SECRET_KEY = '$PDNSADMIN_SECRET_KEY'";fi >>/opt/pdnsadmin/config.py
+if [ -z $PDNSADMIN_PORT ];then echo "PORT = 9191";else echo "PORT = $PDNSADMIN_PORT";fi >>/opt/pdnsadmin/config.py
+if [ -z $PDNSADMIN_SQLA_DB_USER ];then echo "SQLA_DB_USER = 'pda'";else echo "SQLA_DB_USER = '$PDNSADMIN_SQLA_DB_USER'";fi >>/opt/pdnsadmin/config.py
+if [ -z $PDNSADMIN_SQLA_DB_PASSWORD ];then echo "SQLA_DB_PASSWORD = 'changeme'";else echo "SQLA_DB_PASSWORD = '$PDNSADMIN_SQLA_DB_PASSWORD'";fi >>/opt/pdnsadmin/config.py
+if [ -z $PDNSADMIN_SQLA_DB_HOST ];then echo "SQLA_DB_HOST = '127.0.0.1'";else echo "SQLA_DB_HOST = '$PDNSADMIN_SQLA_DB_HOST'";fi >>/opt/pdnsadmin/config.py
+if [ -z $PDNSADMIN_SQLA_DB_PORT ];then echo "SQLA_DB_PORT = 3306";else echo "SQLA_DB_PORT = $PDNSADMIN_SQLA_DB_PORT";fi >>/opt/pdnsadmin/config.py
+if [ -z $PDNSADMIN_SQLA_DB_NAME ];then echo "SQLA_DB_NAME = 'pda'";else echo "SQLA_DB_NAME = '$PDNSADMIN_SQLA_DB_NAME'";fi >>/opt/pdnsadmin/config.py
 
-cat >>/home/pdnsadmin/config.py <<EOF
+cat >>/opt/pdnsadmin/config.py <<EOF
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = 'mysql://'+SQLA_DB_USER+':'+SQLA_DB_PASSWORD+'@'+SQLA_DB_HOST+':'+str(SQLA_DB_PORT)+'/'+SQLA_DB_NAME
 EOF
 
-cd /home/pdnsadmin
+cd /opt/pdnsadmin
 virtualenv flask
 source ./flask/bin/activate
-flask db migrate -m "Init DB"
-./init_data.py
+
+echo "===> DB management"
+if [ ! -d "${DB_MIGRATION_DIR}" ]; then
+  echo "---> Running DB Init"
+  flask db init --directory ${DB_MIGRATION_DIR}
+  flask db migrate -m "Init DB" --directory ${DB_MIGRATION_DIR}
+  flask db upgrade --directory ${DB_MIGRATION_DIR}
+#  ./init_data.py
+else
+  echo "---> Running DB Migration"
+  flask db migrate -m "Upgrade DB Schema" --directory ${DB_MIGRATION_DIR}
+  flask db upgrade --directory ${DB_MIGRATION_DIR}
+fi
+
 echo "===> Update PDNS API connection info"
 # initial setting if not available in the DB
 mysql -h${PDNSADMIN_SQLA_DB_HOST} -u${PDNSADMIN_SQLA_DB_USER} -p${PDNSADMIN_SQLA_DB_PASSWORD} -P${PDNSADMIN_SQLA_DB_PORT} ${PDNSADMIN_SQLA_DB_NAME} -e "INSERT INTO setting (name, value) SELECT * FROM (SELECT 'pdns_api_url',
  '${PDNS_PROTO}://${PDNS_HOST}:${PDNS_PORT}') AS tmp WHERE NOT EXISTS (SELECT name FROM setting WHERE name = 'pdns_api_url') LIMIT 1;"
 mysql -h${PDNSADMIN_SQLA_DB_HOST} -u${PDNSADMIN_SQLA_DB_USER} -p${PDNSADMIN_SQLA_DB_PASSWORD} -P${PDNSADMIN_SQLA_DB_PORT} ${PDNSADMIN_SQLA_DB_NAME} -e "INSERT INTO setting (name, value) SELECT * FROM (SELECT 'pdns_api_key',
  '${PDNS_API_KEY}') AS tmp WHERE NOT EXISTS (SELECT name FROM setting WHERE name = 'pdns_api_key') LIMIT 1;"
-/home/pdnsadmin/run.py
+/usr/bin/gunicorn -t 120 --workers 4 --bind '0.0.0.0:$PDNSADMIN_PORT' --log-level info app:app
